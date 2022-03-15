00d072f6855e24b4e4fa45311e73599d68ec7617
nurhossainfarid <faahsan1516@gmail.com>
2022-03-15 18:57:04 +0600

    again write js

--- js/slider.js
+++ js/slider.js
@@ -10,16 +10,10 @@ const images = [
 let imgIndex = 0;
 const imgElement = document.getElementById('slider-img');
 setInterval(() => {
-    if (imgIndex>= images.length) {
+    if (imgIndex >= images.length) {
         imgIndex = 0;
     }
     const imgUrl = images[imgIndex];
-    imgElement.setAttribute('src', imgUrl)
+    imgElement.setAttribute('src', imgUrl);
     imgIndex++;
-}, 1000)
-
-
-const createSlider = () => {
-    
-};
-createSlider();
+},1500)
